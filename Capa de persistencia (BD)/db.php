db740828df102edc6a774b374e649feec4c1c464
Carlos Ruiz Diaz <carlossebastianruizdiaz@gmail.com>
2026-05-02 13:44:35 -0400

    Refactorización de db.php para ocultar credenciales en config.ini

--- Capa de persistencia (BD)/db.php
+++ Capa de persistencia (BD)/db.php
@@ -1,12 +1,23 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "stockflow_db";
 
-$conn = new mysqli($host, $user, $pass, $db);
 
-if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
+// Ajusta la ruta dependiendo de dónde llames a este archivo
+$ini_path = __DIR__ . '/../config.ini'; 
+
+if (file_exists($ini_path)) {
+    $config = parse_ini_file($ini_path);
+
+    $host = $config['host'];
+    $user = $config['user'];
+    $pass = $config['pass'];
+    $db   = $config['name'];
+
+    $conn = new mysqli($host, $user, $pass, $db);
+
+    if ($conn->connect_error) {
+        die("Error de conexión: " . $conn->connect_error);
+    }
+} else {
+    die("Error: Archivo de configuración no encontrado.");
 }
 ?>
